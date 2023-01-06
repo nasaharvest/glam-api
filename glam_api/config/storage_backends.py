3a6cfb8e8618a374fce25d9bbdb1e2f90391abf6
John <jfkeniston@gmail.com>
2023-01-06 11:30:25 -0500

    Make vector storage public

--- glam_api/config/storage_backends.py
+++ glam_api/config/storage_backends.py
@@ -37,23 +37,9 @@ class RasterStorage(S3Boto3Storage):
 
 
 class VectorStorage(S3Boto3Storage):
-    # https://github.com/jschneier/django-storages/issues/606
-    # "The issue appears to be the new behaviour of django-storages to
-    # automatically pull the AWS security token from environment variables.
-    # AWS Lambda provides AWS_SESSION_TOKEN and AWS_SECURITY_TOKEN as
-    # environment variables,
-    # taken from the execution role for Lambda which may not be the same
-    # credentials required by django-storages for S3 access"
-
-    # Overriding _get_security_token to prevent duplicate security tokens
-    if settings.USE_S3_RASTERS:
-        def _get_security_token(self):
-            return None
-
     location = ''
-    default_acl = 'private'
+    default_acl = 'public-read'
     file_overwrite = True
-    custom_domain = False
 
 
 class ColormapStorage(S3Boto3Storage):
