b7cfa5dcf9a2dba891d3a6f50d453a59bb67050d
John <jfkeniston@gmail.com>
2023-06-30 13:25:49 -0400

    Update export function to handle boundary features

--- glam_api/glam/utils/export.py
+++ glam_api/glam/utils/export.py
@@ -1,4 +1,5 @@
 import os
+import json
 import datetime
 import math
 from django.shortcuts import get_object_or_404
@@ -24,59 +25,69 @@ from affine import Affine
 import pyproj
 from pyproj import CRS
 
-from ..models import (ProductRaster, ImageExport)
+from ..models import ProductRaster, BoundaryLayer, BoundaryFeature, ImageExport
 
 
 def image_export(export_id, data):
-
-    product_id = data.get('product_id', None)
-    date = data.get('date', None)
-    geom = data.get('geom', None)
-    anomaly = data.get('anomaly', None)
-    anomaly_type = data.get('anomaly_type', None)
-    diff_year = data.get('diff_year', None)
-    cropmask = data.get('cropmask_id', None)
-    if cropmask == 'no-mask':
+    product_id = data.get("product_id", None)
+    date = data.get("date", None)
+    geom = data.get("geom", None)
+    layer_id = data.get("layer_id", None)
+    feature_id = data.get("feature_id", None)
+    anomaly = data.get("anomaly", None)
+    anomaly_type = data.get("anomaly_type", None)
+    diff_year = data.get("diff_year", None)
+    cropmask = data.get("cropmask_id", None)
+    if cropmask == "no-mask":
         cropmask = None
 
-    product_queryset = ProductRaster.objects.filter(
-        product__product_id=product_id
-    )
-    product_dataset = get_object_or_404(
-        product_queryset,
-        date=date
-    )
+    product_queryset = ProductRaster.objects.filter(product__product_id=product_id)
+    product_dataset = get_object_or_404(product_queryset, date=date)
 
     if not settings.USE_S3_RASTERS:
         path = product_dataset.file_object.path
     if settings.USE_S3_RASTERS:
         path = product_dataset.file_object.url
 
-    # if feature collection get first feature
-    if geom['type'] == 'FeatureCollection':
-        geom = geom['features'][0]
+    if geom:
+        # if feature collection get first feature
+        if geom["type"] == "FeatureCollection":
+            geom = geom["features"][0]
 
-    geometry = geom['geometry']
-    geom = shape(geometry)
+        geometry = geom["geometry"]
+        geom = shape(geometry)
+    elif layer_id and feature_id:
+        boundary_layer = BoundaryLayer.objects.get(layer_id=layer_id)
+        boundary_features = BoundaryFeature.objects.filter(
+            boundary_layer=boundary_layer
+        )
+        boundary_feature = get_object_or_404(boundary_features, feature_id=feature_id)
+        boundary_feature_name = boundary_feature.feature_name
+        geom = shape(json.loads(boundary_feature.geom.geojson))
 
     with rasterio.open(path) as src:
         product_crs = src.crs
-        wgs84 = pyproj.CRS('EPSG:4326')
+        wgs84 = pyproj.CRS("EPSG:4326")
         project = pyproj.Transformer.from_crs(
-            wgs84, product_crs, always_xy=True).transform
+            wgs84, product_crs, always_xy=True
+        ).transform
         new_geom = transform(project, geom)
         no_data = src.nodata
 
         out_image, out_transform = mask(src, [mapping(new_geom)], crop=True)
         out_meta = src.meta
-        out_meta.update({"driver": "GTiff",
-                         "height": out_image.shape[1],
-                         "width": out_image.shape[2],
-                         "transform": out_transform})
+        out_meta.update(
+            {
+                "driver": "GTiff",
+                "height": out_image.shape[1],
+                "width": out_image.shape[2],
+                "transform": out_transform,
+            }
+        )
 
     cog_options = cog_profiles.get("deflate")
     out_meta.update(cog_options)
-    filename = f'{settings.IMAGE_EXPORT_LOCAL_PATH}/{export_id}.tif'
+    filename = f"{settings.IMAGE_EXPORT_LOCAL_PATH}/{export_id}.tif"
 
     with MemoryFile() as memfile:
         with memfile.open(**out_meta) as mem:
@@ -99,10 +110,10 @@ def upload_export(task):
     export_id = task.result
     export = ImageExport.objects.get(id=export_id)
 
-    filename = f'{settings.IMAGE_EXPORT_LOCAL_PATH}/{export_id}.tif'
+    filename = f"{settings.IMAGE_EXPORT_LOCAL_PATH}/{export_id}.tif"
     print(filename)
     # add file to export object & upload to s3
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         export.file_object = File(f, name=os.path.basename(f.name))
         export.save()
 
