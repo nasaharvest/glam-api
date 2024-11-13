4402111b80d6322872a770bfd3990df4a7d5bdc5
John Keniston <jfkeniston@gmail.com>
2024-11-13 11:16:23 -0500

    remove outdated code

--- glam_api/glam/management/commands/add-boundary-rasters.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from ...utils.ingest import add_boundary_rasters
-
-
-class Command(BaseCommand):
-    help = 'bulk add boundary layer raster datasets'
-
-    def handle(self, *args, **options):
-        add_boundary_rasters()
--- glam_api/glam/management/commands/add-cropmask-rasters.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from ...utils.ingest import add_cropmask_rasters
-
-
-class Command(BaseCommand):
-    help = 'bulk add cropmask raster datasets'
-
-    def handle(self, *args, **options):
-        add_cropmask_rasters()
