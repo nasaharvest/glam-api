63ee31275872eef72834b454cc1c17cc916193c0
John Keniston <jfkeniston@gmail.com>
2024-12-17 15:32:18 -0500

    refactor: add example gdal/goes configuration settings for geodjango

--- glam_api/config/local_settings/dev_settings.py
+++ glam_api/config/local_settings/dev_settings.py
@@ -11,3 +11,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+### GEODJANGO CONFIGURATION OPTIONS ###
+
+# GEOS_LIBRARY_PATH = '/home/bob/local/lib/libgeos_c.so'
+# GDAL_LIBRARY_PATH = '/home/sue/local/lib/libgdal.so'
